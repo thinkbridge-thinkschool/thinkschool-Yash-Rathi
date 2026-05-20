2bc5e10f82f604f9e0023c7df4effc9a1ae45b82
Yash Rathi <yashsunilrathi@gmail.com>
2026-05-20 10:14:17 +0530

    Recreate: thinkschool_day2/piece8_DependencyInjection

--- thinkschool_Day2/Abstractions/IClock.cs
+++
@@ -1,5 +0,0 @@
-namespace QuotesApi.Abstractions;
-public interface IClock
-{
-    DateTimeOffset UtcNow { get; }
-}
--- thinkschool_Day2/Infrastructure/SystemClock.cs
+++
@@ -1,6 +0,0 @@
-using QuotesApi.Abstractions;
-namespace QuotesApi.Infrastructure;
-public class SystemClock : IClock
-{
-    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
-}
